48b59329cd959d998efb36323c494321729e82e7
Onkarnath <onkarnath.rai@nttdata.com>
2017-12-20 16:15:10 +0530

    Changes made by Dev2 user. added 2 extra name in name string

--- TestProject/src/main/java/TestClass.java
+++ TestProject/src/main/java/TestClass.java
@@ -2,7 +2,7 @@
 public class TestClass {
 
 	public static void main(String[] args) {
-		String []names = {"Onkar","Varun","Dharmesh"};
+		String []names = {"Onkar","Varun","Dharmesh","Anuj","Rahul"};
 		for(int i=0; i<names.length; i++) {
         System.out.println("Hello :\n" +names[i]);
 		}
