3bc630568076971fda8daf70abb17a7e8fc19497
Onkarnath <onkarnath.rai@nttdata.com>
2017-12-20 16:08:19 +0530

    changes made in test class

--- TestProject/src/main/java/TestClass.java
+++ TestProject/src/main/java/TestClass.java
@@ -2,7 +2,10 @@
 public class TestClass {
 
 	public static void main(String[] args) {
-System.out.println("Hello Java");
+		String []names = {"Onkar","Varun","Dharmesh"};
+		for(int i=0; i<names.length; i++) {
+        System.out.println("Hello :\n" +names[i]);
+		}
 	}
 
 }
